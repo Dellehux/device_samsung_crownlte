42b6f48eb66b6da71b455a5b621f1388bf2efb11
Phạm Nguyễn Gia Khiêm <phamnguyengiakhiem@gmail.com>
2022-10-08 19:38:25 +0700

    crownlte: Proton+ bringup

--- proton_crownlte.mk
+++ proton_crownlte.mk
@@ -23,6 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit from crownlte device
 $(call inherit-product, device/samsung/crownlte/device.mk)
+PROTONPLUS_BUILD_VARIANT := OFFICIAL
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2960
@@ -30,10 +31,10 @@ TARGET_SCREEN_WIDTH := 1440
 TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/proton/config/common.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_crownlte
+PRODUCT_NAME := proton_crownlte
 PRODUCT_DEVICE := crownlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N960F
